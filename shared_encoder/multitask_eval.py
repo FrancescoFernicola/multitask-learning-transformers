4bbacb60a04b902a41f9076ef67dd0fcd96e43d6
Francesco Fernicola <ffernicola@magamago.sslmit.unibo.it>
2022-01-02 17:54:45 +0100

    Added RMSE metric and adapted code to predict accordingly

--- shared_encoder/multitask_eval.py
+++ shared_encoder/multitask_eval.py
@@ -3,44 +3,43 @@ import nlp
 import numpy as np
 import torch
 import transformers
-
+from datasets import load_metric
+import pandas as pd
 
 def multitask_eval_fn(multitask_model, model_name, features_dict, batch_size=8):
     preds_dict = {}
     tokenizer = transformers.AutoTokenizer.from_pretrained(model_name)
-    for task_name in ["quora_keyword_pairs", "spaadia_squad_pairs"]:
+
+    metric = load_metric("rmse")
+    
+    for idx, task_name in enumerate(["BERTScore", "CushLEPOR", "COMET", "TransQuest"]):
         val_len = len(features_dict[task_name]["validation"])
-        acc = 0.0
+        eval = 0
         for index in range(0, val_len, batch_size):
 
-            batch = features_dict[task_name]["validation"][
-                index : min(index + batch_size, val_len)
-            ]["doc"]
-            labels = features_dict[task_name]["validation"][
-                index : min(index + batch_size, val_len)
-            ]["target"]
-            inputs = tokenizer(batch, max_length=512, padding=True)
-            inputs["input_ids"] = torch.LongTensor(inputs["input_ids"])
-            inputs["attention_mask"] = torch.LongTensor(inputs["attention_mask"])
-            logits = multitask_model("quora_keyword_pairs", **inputs)[0]
+            batch = features_dict[task_name]["validation"][index : min(index + batch_size, val_len)]["doc"]
+            
+            labels = features_dict[task_name]["validation"][index : min(index + batch_size, val_len)]["target"]
+            inputs = tokenizer(batch, max_length=512)
+            
+            inputs["input_ids"] = torch.LongTensor(inputs["input_ids"]).cuda()
+            inputs["attention_mask"] = torch.LongTensor(inputs["attention_mask"]).cuda()
+            
+            logits = multitask_model(task_name, **inputs).logits
+            
+            predictions = torch.argmax(torch.FloatTensor(torch.softmax(logits, dim=1).detach().cpu().tolist()),dim=1)           
+                     
+            metric.add_batch(predictions=predictions, references=np.array(labels))
 
-            predictions = torch.argmax(
-                torch.FloatTensor(torch.softmax(logits, dim=1).detach().cpu().tolist()),
-                dim=1,
-            )
-            acc += sum(np.array(predictions) == np.array(labels))
-        acc = acc / val_len
-        print(f"Task name: {task_name} \t Accuracy: {acc}")
-    """
-    print(eval_dataloader.data_loader.collate_fn)
-    preds_dict[task_name] = trainer.prediction_loop(
-        eval_dataloader,
-        description=f"Validation: {task_name}",
-    )
-    for x in eval_dataloader:
-        print(x)
-    # Evalute task
-    nlp.load_metric("glue", name="rte").compute(
-        np.argmax(preds_dict[task_name].predictions),
-        preds_dict[task_name].label_ids,
-    )"""
+            print(f"\nRMSE value for current batch: {metric.compute()['rmse']}")
+            
+            eval += metric.compute()["rmse"]
+            
+            print("\nCurrent total RMSE value: {eval}")
+        
+        eval = eval/val_len
+        preds_dict[task_name] = eval
+        print(f"\nTask name: {task_name}\tFinal RMSE: {eval}\n\n")
+    
+    preds = pd.DataFrame.from_dict(preds_dict)
+    print(preds)
